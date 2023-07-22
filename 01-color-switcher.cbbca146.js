4d9be6abdb39d0208877734d7a2a2d695c2588c9
romanfliak <125312720+romanfliak@users.noreply.github.com>
2023-07-22 14:46:25 +0000

    Deploying to gh-pages from @ romanfliak/goit-js-hw-09@003552edaf33d5eca25c192f12648ebdd7427329 🚀

---
+++ 01-color-switcher.cbbca146.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),a=document.querySelector("body");t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n=setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(n)}));var n=null;e.disabled=!1}();
+//# sourceMappingURL=01-color-switcher.cbbca146.js.map
