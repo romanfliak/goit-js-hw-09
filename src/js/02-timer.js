ff40e97c2bdca0d8374dba75661b0d733b798bc6
romanfliak <rfliak568@gmail.com>
2023-07-23 17:28:43 +0200

    .

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -10,32 +10,33 @@ const hours =document.querySelector('span[data-hours]');
 const minutes =document.querySelector('span[data-minutes]');
 const seconds = document.querySelector('span[data-seconds]');
 
-
-btnStart.classList.add('button-inactive');
 btnStart.addEventListener('click', startTimer);
-
+btnStart.disabled = true;
 const currentDate = new Date();
-let dateForUser
+let dateForUser;
 
-flatpickr(inputText,{
+const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: currentDate,
     minuteIncrement: 1,
-    onClose( selectedDate){
+    onClose(selectedDate) {
+        dateForUser =selectedDate[0]
         if(currentDate > dateForUser){
             Notiflix.Notify.failure('please choose a date in the future');
             return
         }else{
-            btnStart.classList.remove('button-inactive')
+           btnStart.disabled = false;
         }
     }
-})
+}
+
+flatpickr(inputText , options )
 
 function startTimer() {
     const id = setInterval(()=>{
         const current = new Date()
-        const timerTime = conversMs( dateForUser - current);
+        const timerTime = convertMs(dateForUser - current)
         if (timerTime.seconds >= 0) {
             days.textContent = timerTime.days.toString().padStart(2,'0');
             hours.textContent = timerTime.hours.toString().padStart(2,'0');
@@ -45,46 +46,24 @@ function startTimer() {
             clearInterval(id);
             Notiflix.Notify.success('Congratulation! Time is ower.')
         }
-    })
+    }, 1000)
+
 }
+function convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const options = {
-//     enableTime: true,
-//     time_24hr: true,
-//     defaultDate: new Date(),
-//     minuteIncrement: 1,
-//     onClose(selectedDates) {
-//     const selectedDate = (selectedDates[0]);
-
-//     if (selectedDate <= new Date()) {
-//         window.alert('please chose a date in the future');
-//         btn.disabled = true;
-//     }else{
-//         btn.addEventListener('click',startTimer);
-//         btn.disabled = false;
-//     }
-//     },
-//   };
-
-//   const flatpickrInstance = flatpickr('#datetime-picker',options);
-
+}
 
 
 
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,55 @@
+import Notiflix from 'notiflix';
+console.log(Notiflix)
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise ((resolve, reject) =>{
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(()=> {
+      if (shouldResolve) {
+        resolve({position,delay})
+      } else {
+       reject({position, delay});
+      }
+    }, delay)
+  
+  })
+ 
   }
+
+ 
+
+const form = document.querySelector('.form')
+const inputDelay = document.querySelector('input[name="delay"]');
+const inputStep = document.querySelector('input[name="step"]');
+const inputAmount = document.querySelector('input[name="amount"]')
+
+form.addEventListener('submit',handlerSubmit);
+ 
+
+function handlerSubmit(event) {
+  event.preventDefault();
+
+  const firstDelay = parseInt(inputDelay.value);
+  const step = parseInt(inputStep.value);
+  const amount = parseInt(inputAmount.value);
+
+
+  for (let i = 0; i < amount; i++) {
+    const position = i + 1;
+    const delay = firstDelay + i * step;
+
+
+  createPromise(position,delay)
+  .then(({position,delay})=> {
+    Notiflix.Notify.success(
+      ` ✔️ fullfilled promise ${position} in ${delay}`
+    );
+  })
+  .catch(({position,delay})=>{
+    Notiflix.Notify.failure(
+`      ✖️ Rejected promise ${position} in ${delay}ms `    )
+  })
+  }
+  form.reset()
 }
